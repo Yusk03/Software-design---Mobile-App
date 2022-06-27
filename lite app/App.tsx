6258fa628fa25b808a97a1f65dae8288376e569c
fr0staman <mrfrostaman@gmail.com>
2022-06-27 15:05:51 +0300

    Update Mobile App to diploma version

--- lite app/App.tsx
+++ lite app/App.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { persistor, store } from "./src/redux/store";
 import { ActivityIndicator, StyleSheet, View } from "react-native";
+
+import { persistor, store } from "./src/redux/store";
 import { colors } from "./src/styles";
 import AppView from "./src/modules/AppView";
 import AppContextProvider from "./src/modules/AppContextProvider";
